4339d7e0baccdee7a500307f8d6a6078c60be716
SUDHITHA4225 <147793010+SUDHITHA4225@users.noreply.github.com>
2024-01-04 19:00:30 +0530

    #include<stdio.h> int main() {     int n,i,j,k;     scanf("%d",&n);     for(i=1; i<=n; i++)     {         for(j=1; j<=n; j++)         {             if((i==j)||(j==n-i+1)) printf("%c",64+i);             else  printf(" ");         }         printf(" ");     } }

---
+++ Pattern_8.4.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int n,i,j,k;
+    scanf("%d",&n);
+    for(i=1; i<=n; i++)
+    {
+        for(j=1; j<=n; j++)
+        {
+            if((i==j)||(j==n-i+1)) printf("%c",64+i);
+            else  printf(" ");
+        }
+        printf("
+");
+    }
+}
